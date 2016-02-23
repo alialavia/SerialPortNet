09c6e074fb158b4fbfd1bc25d3b1bcb17f38e8ab
Ali Alavi <alialavia@gmail.com>
2016-02-23 14:38:55 +0100

    Documentation added

---
+++ SerialDataReceivedEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace SerialPortNET
+{
+    /// <summary>
+    /// Represents the method that will handle the DataReceived event of a SerialPort object.
+    /// </summary>
+    /// <param name="sender">The sender of the event, which is the <see cref="SerialPort"/> object.</param>
+    /// <param name="e">A <see cref="SerialDataReceivedEventArgs"/> object that contains the event data.</param>
+    public delegate void SerialDataReceivedEventHandler(object sender, SerialDataReceivedEventArgs e);
+}
